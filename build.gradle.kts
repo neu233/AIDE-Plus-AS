71e222af9462b18d7ab3a45ed5bd1cd8e6fc4d4c
neu233 <3115093767@qq.com>
2024-12-08 17:23:51 +0100

    工作流程修复

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") apply false version "8.7.3"
@@ -6,4 +8,7 @@ plugins {
 }
 
 
-
+allprojects {
+    //noinspection GrDeprecatedAPIUsage
+    buildDir = file("${rootProject.rootDir.path}/build/${project.path.substring(1).replace(':', '-')}")
+}
